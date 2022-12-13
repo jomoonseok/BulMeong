dfe422bb20e308727e4e3b81fa64e40b159bc3d7
seratpfk <sera7057@naver.com>
2022-12-13 17:51:47 +0900

    13

---
+++ bulmeong/src/main/java/com/gdu/bulmeong/cart/controller/CartController.java
@@ -0,0 +1,56 @@
+package com.gdu.bulmeong.cart.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.gdu.bulmeong.cart.domain.CartDTO;
+import com.gdu.bulmeong.cart.service.CartService;
+import com.gdu.bulmeong.users.domain.UsersDTO;
+
+
+@Controller
+public class CartController {
+
+	@Autowired
+	private CartService cartService;
+	 
+	@GetMapping("/cart")
+	public String cart() {
+		return "cart/addCart";
+	}
+	
+	// 카트 담기
+	@ResponseBody
+	@RequestMapping(value = "/cart/addCart", method = RequestMethod.POST)
+	public int addCart(CartDTO cart, HttpSession session) {
+	   int result = 0;
+	   
+	   UsersDTO user = (UsersDTO)session.getAttribute("user");
+	   
+	   if(user != null) {
+		   cart.setId(user.getId());
+		   cartService.addCart(cart);
+		   result = 1;
+	   }
+	   
+	   return result;
+	   
+	}
+	
+
+	
+	
+		
+	
+		
+
+
+	
+}
---
+++ bulmeong/src/main/java/com/gdu/bulmeong/cart/service/CartService.java
@@ -0,0 +1,7 @@
+package com.gdu.bulmeong.cart.service;
+
+import com.gdu.bulmeong.cart.domain.CartDTO;
+
+public interface CartService {
+	public void addCart(CartDTO cart);
+}
