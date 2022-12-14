7b9dac5162ae01ee958a248eb21531e16b56dcf0
jhw3603 <jhw3603@naver.com>
2022-12-14 20:53:02 +0900

    팝업창..

--- bulmeong/src/main/java/com/gdu/bulmeong/reserve/controller/ReserveController.java
+++ bulmeong/src/main/java/com/gdu/bulmeong/reserve/controller/ReserveController.java
@@ -2,6 +2,8 @@ package com.gdu.bulmeong.reserve.controller;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,8 +39,9 @@ public class ReserveController {
 	 */
 	
 	
-	@GetMapping("/reserve/pay")
-	public String reserve_pay() {
+	@PostMapping("/reserve/pay")
+	public String reserve_pay(HttpServletRequest request) {
+		System.out.println(request.getParameter("tentNo"));
 		return "reserve/pay";
 	}
 	
