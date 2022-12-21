b81959584926922c442aa8d4a6fe5507a14cdb2e
hw <hwcho1205@naver.com>
2022-12-21 18:03:11 +0900

    1221/한게없삼;

--- bulmeong/src/main/java/com/gdu/bulmeong/config/WebMvcConfig.java
+++ bulmeong/src/main/java/com/gdu/bulmeong/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/load/image/**")
-			.addResourceLocations("file:///C:/noticeImage/");
+			.addResourceLocations("//");
 	}
 	
 }
