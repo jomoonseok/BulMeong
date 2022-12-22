af3fb1bc326f4645b320fb2a513a0eaa2a4711ec
hyewon <hwcho1205@naver.com>
2022-12-23 00:57:06 +0900

    1223/qnalist갈아엎긔

--- bulmeong/src/main/java/com/gdu/bulmeong/faq/controller/FaqController.java
+++ bulmeong/src/main/java/com/gdu/bulmeong/faq/controller/FaqController.java
@@ -2,6 +2,7 @@ package com.gdu.bulmeong.faq.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.gdu.bulmeong.faq.service.FaqService;
 
@@ -11,7 +12,10 @@ public class FaqController {
 	@Autowired 
 	private FaqService faqService;
 	 
-	
+	@GetMapping("/faq/list")
+	public String faqList() {
+		return "faq/list";
+	}
 	
 	
 	
