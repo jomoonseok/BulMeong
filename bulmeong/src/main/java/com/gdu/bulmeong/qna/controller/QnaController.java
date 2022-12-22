9868c534af90de78ca1c2f65f643736e20f1519f
hyewon <hwcho1205@naver.com>
2022-12-22 11:51:49 +0900

    1222/오전

--- bulmeong/src/main/java/com/gdu/bulmeong/qna/controller/QnaController.java
+++ bulmeong/src/main/java/com/gdu/bulmeong/qna/controller/QnaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.gdu.bulmeong.qna.service.QnaService;
 
@@ -32,6 +33,7 @@ public class QnaController {
 	public String popup() {
 		return "qna/popup";
 	}
+	
 	
 	
 }
